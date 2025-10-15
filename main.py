4b3bf643f4221b2b23ec8218f635d6bfe8bfa842
Antonov Mikhail <mixail.antonoff2018@yandex.ru>
2025-10-15 16:32:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,29 +6,29 @@ BLUE = '\u001b[44m'
 END = '\u001b[0m'
 GREEN = '\u001b[42m'
 YELLOW = '\u001b[43m'
-BLACK = '\u001b[0;0;0m'
+BLACK = '\u001b[0;30m'
 
 #Япония
+k = 10
 
-k = 4
 def Japan():
-    for i in range(-2, 15+1+2): 
-        if i==3 or i==12: 
-            print(f'{WHITE}{"  " * (13+k)}{RED}{"  " * (9)}{WHITE}{"  " * (13+k)}{END}')
-        elif i==4 or i== 11:
-            print(f'{WHITE}{"  " * (12+k)}{RED}{"  " * (11)}{WHITE}{"  " * (12+k)}{END}')
-        elif i==5 or i==10:
-            print(f'{WHITE}{"  " * (11+k)}{RED}{"  " * (13)}{WHITE}{"  " * (11+k)}{END}')
-        elif 6<=i<=9:
-            print(f'{WHITE}{"  " * (11+k)}{RED}{"  " * (13)}{WHITE}{"  " * (11+k)}{END}')
+    for i in range(-2, 15 + 1 + 2): 
+        if i == 3 or i == 12: 
+            print(f'{WHITE}{"  " * (13 + k)}{RED}{"  " * (9)}{WHITE}{"  " * (13 + k)}{END}')
+        elif i == 4 or i == 11:
+            print(f'{WHITE}{"  " * (12 + k)}{RED}{"  " * (11)}{WHITE}{"  " * (12 + k)}{END}')
+        elif i == 5 or i == 10:
+            print(f'{WHITE}{"  " * (11 + k)}{RED}{"  " * (13)}{WHITE}{"  " * (11 + k)}{END}')
+        elif 6 <= i <= 9:
+            print(f'{WHITE}{"  " * (11 + k)}{RED}{"  " * (13)}{WHITE}{"  " * (11 + k)}{END}')
         else:
-            print(f'{WHITE}{"  " * (35+2*k)}{END}')
+            print(f'{WHITE}{"  " * (35 + 2 * k)}{END}')
 
 #Узор
 def uzor():
-    n=4
-    for j in range(1,n+1):
-        for i in range(0,12):
+    n = 4
+    for j in range(1, n + 1):
+        for i in range(0, 12):
             if i == 0 or i == 10:
                 print(f'{WHITE}{"  " * 4}{BLUE}{"  " * 3}{WHITE}{"  " * 8}{BLUE}{"  " * 3}{WHITE}{"  " * 4}{END}')
             elif i == 1 or i == 9:
@@ -45,14 +45,15 @@ def uzor():
 #График
 
 def graph(): 
-    s=' '
-    print('График функции', " " * 2,'y=3*x')
-    for i in range(0,11):
-        if i<7:
+    s = ' '
+    print('График функции', " " * 2, 'y=3*x')
+    print()
+    for i in range(0, 11):
+        if i < 7:
             print(str(30 - 3 * i)," ",f'{WHITE}{"  " * (10 - i)}{GREEN}{"  " * 1}{WHITE}{"  " * i}{END}')
         else:
             print(str(30 - 3 * i),"  ",f'{WHITE}{"  " * (10 - i)}{GREEN}{"  " * 1}{WHITE}{"  " * i}{END}')
-        s=s+str(i)+ " "
+        s=s + str(i) + " "
     print(" " * 4, s)
 
 #Диаграммма
@@ -63,19 +64,20 @@ def diag():
     menshe = 0
     for el in file:
         el = float(el)
-        if el<=0:
-            if abs(el)<5:
-                bolshe+=1
-            elif abs(el)!=5:
-                menshe+=1
-    per_b= int(bolshe*100/(bolshe+menshe))
-    per_m=100-per_b
+        if el <= 0:
+            if abs(el) < 5:
+                bolshe += 1
+            elif abs(el) != 5:
+                menshe += 1
+    per_b = int(bolshe * 100 / (bolshe + menshe))
+    per_m = 100 - per_b
+    file.close()
 
     for i in range(2): 
         if i == 0: 
-            print('Больше -5:', f'{GREEN}{' '*per_b}{END}', str(per_b)+'%')
+            print('Больше -5:', f'{GREEN}{"  " * per_b}{END}', str(per_b) + '%')
         else: 
-            print('Меньше -5:', f'{YELLOW}{' '*per_m}{END}', str(per_m)+'%')
+            print('Меньше -5:', f'{YELLOW}{"  " * per_m}{END}', str(per_m) + '%')
 
 Japan()
 print()
@@ -84,4 +86,3 @@ print()
 graph()
 print()
 diag()
-
---
+++ pattern2.py
@@ -0,0 +1,44 @@
+import os
+import time
+
+def charge_animation():
+    frames = [
+        """
+        =========
+        |       |
+        |  10%  |
+        |       |
+        =========
+        """,
+        """
+        ===========
+        ||       ||
+        ||  50%  ||
+        ||       ||
+        ===========
+        """,
+        """
+        =============
+        |||       |||
+        |||  90%  |||
+        |||       |||
+        ============= 
+
+        """,
+        """
+        *************
+        *           *
+        *  CHARGED! *
+        *           *
+        *************
+        """
+    ]
+    
+    for frame in frames:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Charge:")
+        print(frame)
+        time.sleep(1)
+
+# Запускаем обратный отсчет
+charge_animation()
